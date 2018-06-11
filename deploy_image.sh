a457542a226f0ce6f5bee1021fc5ebfcb8d752e2
Nielson Santana <nielsonnas@gmail.com>
2018-06-11 13:44:38 +0000

    Changes in .travis.yml. Added deploy_image.sh

---
+++ deploy_image.sh
@@ -0,0 +1,5 @@
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t $IMAGE_NAME $DOCKER_BUILD_DIR
+docker tag $IMAGE_NAME $IMAGE_NAME-$VERSION
+docker push $IMAGE_NAME
+docker push $IMAGE_NAME-$VERSION
