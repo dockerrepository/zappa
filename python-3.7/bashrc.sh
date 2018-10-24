6e6e8cc0dce1b27c300ae5f8aaaf2267a7de37c5
Nielson Santana <nielsonnas@gmail.com>
2018-10-24 07:27:55 +0000

    Added python-3.7

---
+++ python-3.7/bashrc.sh
@@ -0,0 +1,33 @@
+
+# initialize a python virtual environ
+function init_venv(){
+  # Create venv if not exists
+  if [ ! -f /var/venv/bin/activate ]; then
+      echo "Creating virtualenv (/var/venv)."
+      python3 -m venv /var/venv
+      echo "done."
+  fi
+}
+
+# Activate the venv
+function activate_venv(){
+  source /var/venv/bin/activate
+}
+
+function install_zappa_in_venv(){
+  activate_venv
+  installed_zappa_version=$(pip freeze | grep zappa)
+  requirments_zappa_version=$(cat /root/requirements.txt | grep zappa)
+  if [ "$installed_zappa_version" != "$requirments_zappa_version" ]; then
+      echo "Installing zappa($requirments_zappa_version) in virtualenv(/var/venv)."
+      pip install -r /root/requirements.txt > /root/pip-log-requirements.txt
+      echo "Logs: /root/pip-log-requirements.txt"
+      echo "done."
+  fi
+}
+
+init_venv
+
+activate_venv
+
+install_zappa_in_venv
